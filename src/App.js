6b6871bf4ec63ce6a46fab19e4468bf62e2924d8
E-shopee Card E-commerce base website <https://github.com/RIYAZBAIG>
2025-02-06 12:25:22 +0530

    Create TodoList with v2 vercel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import CreditScoringCard from './Components/Pages/CreditScoringCard';
+import TodoList from './Components/Pages/TodoList';
 
 
 function App() {
   return (
     <div className="App">
       <CreditScoringCard/>
+      <TodoList/>
     </div>
   );
 }
---
+++ src/Components/Pages/TodoList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList = () => {
+  return (
+    <div>
+      <h1>Hello TodoList here.....</h1>
+    </div>
+  );
+}
+
+export default TodoList;
